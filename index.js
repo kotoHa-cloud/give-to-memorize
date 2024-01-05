e99db1e940402cd2f40a85ea52f2a27163f0bae2
laxiy <forever.1025@outlook.jp>
2024-01-06 02:29:59 +0900

    set Activity

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, GatewayIntentBits, Events } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds
+    ]
+});
+
+client.once(Events.ClientReady, () => {
+    console.log('bot is ready.');
+    status();
+});
+
+async function status() {
+    client.user.setActivity({
+        name: 'botは正常に起動しています！(ver:0.0.1)',
+        status: 'online'
+    });
+};
+
+client.login(token);
